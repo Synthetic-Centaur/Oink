3a2c2dbf0a55bcbb3d8ec2e07fd18169d4b63955
tlevin <toddlevin802@gmail.com>
2015-12-03 19:11:42 -0800

    Initial customization of routes from boilerplate

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ import 'babel-core/polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import configureStore from '../common/store/configureStore'
-import App from '../common/containers/App'
+import configureStore from './store/configureStore'
+import App from './containers/App'
 
 const initialState = window.__INITIAL_STATE__
 const store = configureStore(initialState)
