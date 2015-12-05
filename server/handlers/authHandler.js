037349d6336f4e9a01d91ec234ecda9934f29819
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-12-05 12:38:02 -0800

    User controller and handler in place and working for sign-up route

--- server/handlers/authHandler.js
+++ server/handlers/authHandler.js
@@ -1,19 +1,40 @@
+import authController from '../controllers/authController'
+
 let authHandler = {
   login(req, res) {
-    console.log("inside login handler");
-    res.send();
+    console.log("inside login handler")
+    res.send()
   },
   logout(req, res) {
-    console.log("inside logout handler");
-    res.send("Success");
+    console.log("inside logout handler")
+    res.send()
   },
   signup(req, res) {
-    console.log("inside signup handler");
-    res.send();
+    console.log("inside signup handler")
+    // assuming user passed as obj in req.body
+    let user = req.body
+    console.log(user)
+    authController.findUser(user)
+    .then((result) => {
+      if (!result) {
+        // if user is not in db add the user
+        authController.addUser(user).then ((newUser) => {
+          if (newUser) {
+            // ToDo: need to log newUser in --> call login controller
+            res.json(newUser)
+          } else {
+            res.sendStatus(500)
+          }
+        })
+      } else {
+        // user is already in database
+        res.sendStatus(409)
+      }
+    })
   },
   authenticate(req, res) {
-    console.log("inside authenticate handler");
-    res.send();
+    console.log("inside authenticate handler")
+    res.send()
   }
 }
 
