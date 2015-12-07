d492f95c72a745a98ac9a79167424fddb42afe31
Clayton Schneider <claykschneider@gmail.com>
2015-12-07 14:44:32 -0800

    Refactor to fetch API instead of axios. Create authKeys file for plaid and other private keys. Move action creators specific to authentication into client/api folder

---
+++ client/routes/routes.js
@@ -0,0 +1,17 @@
+/**************
+TODO: Refactor from index
+****************/
+// import React from 'react';
+// import { Route } from 'react-router';
+
+// import Splash from '../containers/Splash';
+// import Signup from '../containers/Signup';
+// import Plaid from '../containers/Plaid';
+
+
+// export default (
+//   <Route path="/" component={Splash} />
+//   <Route path="/signup" component={Signup}/>
+//   <Route path="/plaid" component={Plaid}/>
+// )
+
