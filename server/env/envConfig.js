7c3e49b31da971d0838b41b38635ca6c131e6af0
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-12-07 15:13:05 -0800

    Added twilio messaging logic in test '/message' route. Will move this logic out of route to be triggered by webhook.

---
+++ server/env/envConfig.js
@@ -0,0 +1,7 @@
+// Twilio Account information
+const twilio_private = {
+  accountSid: 'AC804a5a4ef06f716564a1cabbdd115b15',
+  authToken: 'd855f7bd1cd12a9fa785fcdeeebc02cf'
+}
+
+export default twilio_private
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,8 +5,6 @@ import authHandler from '../handlers/authHandler'
 import apiHandler from '../handlers/apiHandler'
 import passport from 'passport'
 
-
-
 app.post('/auth/login', authHandler.login)
 
 app.get('/auth/logout', authHandler.logout)
@@ -19,10 +17,4 @@ app.get('/api/intitialState', apiHandler.intitialState)
 
 app.post('/api/budget/category/:id', apiHandler.budget)
 
-// app.get('/test', (req, res) => {
-//     let user = new User({first_name: 'Lucilla', last_name: 'Chalmer', phone_number: 7344749351, uuid: 'abc123'})
-//     user.save().then((user) => {
-//       console.log('user has been created!!! YAY!!!')
-//       res.json(user)
-//     })
-// })
+app.get('/message', apiHandler.message)
