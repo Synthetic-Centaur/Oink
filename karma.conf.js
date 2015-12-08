c72a3013792d3b2a61694dbe99584b81c7b48aad
tlevin <toddlevin802@gmail.com>
2015-12-07 16:14:40 -0800

    Karma testing working

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,60 @@
-var webpack = require('webpack');
+var path = require('path');
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    browsers: [ 'Chrome' ], //run in Chrome
-    singleRun: true, //just run once by default
-    frameworks: [ 'mocha' ], //use the mocha test framework
+    basePath: '',
+    browsers: [ 'PhantomJS' ],
     files: [
-      'tests.webpack.js' //just load this file
+      'test/loadtests.js',
     ],
-    plugins: [ 'karma-chrome-launcher', 'karma-chai', 'karma-mocha',
-      'karma-sourcemap-loader', 'karma-webpack', 'karma-coverage',
-      'karma-mocha-reporter'
-    ],
-    preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+    port: 8080,
+    captureTimeout: 60000,
+    frameworks: [ 'phantomjs-shim', 'mocha', 'chai' ],
+    client: {
+      mocha: {}
     },
-    reporters: [ 'mocha', 'coverage' ], //report results in this format
-    webpack: { //kind of a copy of your webpack config
-      devtool: 'inline-source-map', //just do inline source maps instead of the default
+    singleRun: true,
+    reporters: [ 'mocha', 'coverage' ],
+    preprocessors: {
+      'test/loadtests.js': [ 'webpack', 'sourcemap'],
+    },
+    webpack: {
+      devtool: 'eval',
       module: {
-        loaders: [
-          { test: /\.js$/,  loader: 'babel-loader' }
+        preLoaders: [
+          {
+            test: /\.(js|jsx)$/,
+            loader: 'isparta-instrumenter-loader',
+            include: [
+              path.join(__dirname, '/client')
+            ]
+          }
         ],
-        postLoaders: [ { //delays coverage til after tests are run, fixing transpiled source coverage error
-            test: /\.js$/,
-            exclude: /(test|node_modules|bower_components)\//,
-            loader: 'istanbul-instrumenter' } ]
+        loaders: [
+          {
+            test: /\.(png|jpg|gif|woff|woff2|css|sass|scss|less|styl)$/,
+            loader: 'null-loader'
+          },
+          {
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            include: [
+              path.join(__dirname, '/client'),
+              path.join(__dirname, '/test')
+            ]
+          }
+        ]
       }
     },
     webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
+      noInfo: true
     },
     coverageReporter: {
-      type: 'html', //produces a html document after code is run
-      dir: 'coverage/' //path to created html doc
+      dir: 'coverage/',
+      reporters: [
+        { type: 'html' },
+        { type: 'text' }
+      ]
     }
   });
 };
