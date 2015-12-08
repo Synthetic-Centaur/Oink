967bb9ed7ffeef859216e40d4b8ae3cb62264cbd
Aaron Ackerman <a.ackerman050@gmail.com>
2015-12-07 16:00:40 -0800

    Added form verifaction for budget category

--- client/components/home/BudgetCategories.js
+++ client/components/home/BudgetCategories.js
@@ -3,7 +3,26 @@ import DropDownMenu from 'material-ui/lib/drop-down-menu'
 import TextField from 'material-ui/lib/text-field'
 import RaisedButton from 'material-ui/lib/raised-button'
 
-class budgetCategories extends Component {
+class budgetCategories extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {floatingError2Text: 'This field must be numeric.'};
+  }
+
+  handleFloating2ErrorInputChange(e) {
+    // var input = this.refs.amount.getValue()
+    // var z1 = /^[0-9]*$/;
+    // if (!z1.test(input)) { 
+
+    // }
+    var value = e.target.value;
+    var isNumeric = !isNaN(parseFloat(value)) && isFinite(value);
+    console.log(isNumeric);
+    this.setState({
+      floatingError2Text: isNumeric ? '' : 'This field must be numeric.'
+    })
+  }
+
   render() {
     let menuItems = [
       { payload: '1', text: 'Categories'}
@@ -14,14 +33,22 @@ class budgetCategories extends Component {
           <DropDownMenu menuItems={menuItems}/>
         </div>
         <div className="row">
-          <TextField ref="amount" hintText="Enter a sum"/>
+          <TextField 
+            ref="amount" 
+            hintText="Enter a sum"
+            errorText={this.state.floatingError2Text}
+            defaultValue="abc"
+            onChange={this.handleFloating2ErrorInputChange.bind(this)} />
         </div>
         <div className="row">
-          <RaisedButton label="Add new category"/>
+          <RaisedButton 
+            label="Add new category"
+            disabled={this.state.floatingError2Text} />
         </div>
       </form>
     )
   }
+
 }
 
 export default budgetCategories
