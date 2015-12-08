d646ead8a04027ba85277971dcda7553963b2a12
Aaron Ackerman <a.ackerman050@gmail.com>
2015-12-07 17:12:06 -0800

    Added form validation for categories

--- client/components/home/BudgetCategories.js
+++ client/components/home/BudgetCategories.js
@@ -6,44 +6,69 @@ import RaisedButton from 'material-ui/lib/raised-button'
 class budgetCategories extends React.Component{
   constructor (props) {
     super(props);
-    this.state = {floatingError2Text: 'This field must be numeric.'};
+    this.state = {
+      numError: true,
+      catError: true,
+      numErrorText: 'Please enter a number.',
+      catErrorText: 'Please choose a category.'
+    };
   }
 
-  handleFloating2ErrorInputChange(e) {
-    // var input = this.refs.amount.getValue()
-    // var z1 = /^[0-9]*$/;
-    // if (!z1.test(input)) { 
-
-    // }
+  handleNumError(e) {
     var value = e.target.value;
     var isNumeric = !isNaN(parseFloat(value)) && isFinite(value);
-    console.log(isNumeric);
     this.setState({
-      floatingError2Text: isNumeric ? '' : 'This field must be numeric.'
+      numError: isNumeric ? false : true
     })
+  }
+
+  handleCatError(e) {
+    var value = e.target.value;
+    this.setState({
+      catError: value > 1 ? false : true
+    })
+  }
+
+  handleBudget(e) {
+    e.preventDefault
+
+    let category = this.refs.category.getValue()
+    let budget = this.refs.amount.getValue()
+
+    this.props.postBudget({
+      category: category,
+      buget: budget
+    });
   }
 
   render() {
     let menuItems = [
-      { payload: '1', text: 'Categories'}
+      { payload: '1', text: 'Choose a category'},
+      { payload: '2', text: 'Option 1'},
+      { payload: '3', text: 'Option 2'}
     ]
     return (
       <form>
         <div className="row">
-          <DropDownMenu menuItems={menuItems}/>
+          <DropDownMenu 
+            ref="category"
+            menuItems={menuItems}
+            errorText={this.state.catErrorText}
+            onChange={this.handleCatError.bind(this)} />
         </div>
         <div className="row">
           <TextField 
             ref="amount" 
             hintText="Enter a sum"
-            errorText={this.state.floatingError2Text}
+            errorText={this.state.numErrorText}
             defaultValue="abc"
-            onChange={this.handleFloating2ErrorInputChange.bind(this)} />
+            onChange={this.handleNumError.bind(this)} />
         </div>
         <div className="row">
           <RaisedButton 
             label="Add new category"
-            disabled={this.state.floatingError2Text} />
+            disabled={this.state.numError || this.state.catError}
+            onClick={this.handleBudget.bind(this)} />
         </div>
       </form>
     )
