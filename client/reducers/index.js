02f456cf2735831fd8102b9f80428d87d7b8e13d
Clayton Schneider <claykschneider@gmail.com>
2015-12-07 17:31:05 -0800

    Working on landing page refactor

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux'
 import { routeReducer } from 'redux-simple-router';
-import { asyncStatusReducer } from './reducers'
+import { asyncStatusReducer, splashPageReducer } from './reducers'
 
 const rootReducer = combineReducers({
   asyncStatus: asyncStatusReducer,
+  splashPage: splashPageReducer,
   routing: routeReducer
 });
 
