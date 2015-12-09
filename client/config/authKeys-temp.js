267e52e553fd7abf25e9785737965f6b74f9f9bb
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-12-09 11:20:46 -0800

    Added authKeys-temp --> each dev will need create personal authKeys file to hold secrets

---
+++ client/config/authKeys-temp.js
@@ -0,0 +1,15 @@
+/******************************************************
+TODO:
+1) Create a new file called authKeys.js in this folder
+2) Copy this file into authKeys.js, and replace the
+   the necessary keys
+
+Note: authKeys.js will always be git ignored
+******************************************************/
+let authKeys = {
+  plaid: {
+    public_key: 'PLAID_PUBLIC_KEY'
+  }
+}
+
+export default authKeys
