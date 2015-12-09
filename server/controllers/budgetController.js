2f3ffdfe42b184fb730ec9661811944abe8893d3
tlevin <toddlevin802@gmail.com>
2015-12-09 15:07:54 -0800

    Connected front end to backend for budget

--- server/controllers/budgetController.js
+++ server/controllers/budgetController.js
@@ -1,6 +1,25 @@
+import Budget from '../db/models/budget'
+
 let budgetController = {
   createBudget(categoryId, userId, amount){
-    
+    let newBudget = {
+      amount,
+      category_id: categoryId,
+      user_id: userId
+    }
+    //sum all transactions, set to variable actual
+    let actual = 0
+    newBudget.actual = actual
+    //save to table
+    newBudget = new Budget(newBudget)
+    return newBudget.save().then((budget) => {
+      if(budget){
+        return budget
+      } else {
+        return null
+      }
+    })
+    //return budget
   },
   updateBudget(){
 
--- server/handlers/apiHandler.js
+++ server/handlers/apiHandler.js
@@ -1,6 +1,6 @@
 import apiController from '../controllers/apiController'
 import budgetController from '../controllers/budgetController'
-import authController form '../controllers/authController'
+import authController from '../controllers/authController'
 
 let apiHandler = {
   intitialState(req, res) {
@@ -9,7 +9,7 @@ let apiHandler = {
   },
   budget(req, res) {
     console.log("inside budget handler");
-    authController(req).then((user) => {
+    authController.findUserByToken(req).then((user) => {
       if(user) {
         budgetController.createBudget(req.params.id, user.attributes.id, req.body.amount).then( (budget) => {
           if(budget){
@@ -22,7 +22,6 @@ let apiHandler = {
         res.sendStatus(500)
       }
     })
-    res.send(req);
   },
   message(req, res) {
     apiController.sendMessage('I can\'t feel my face when i\'m with you', 7344749351)
