4f186fab311bd29057102a80116ba7115d864a4b
Clayton Schneider <claykschneider@gmail.com>
2015-12-09 20:24:01 -0800

    Add RDS_DB_NAME to env

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -12,7 +12,8 @@ const config = knex({
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
     user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME
   }
 })
 
