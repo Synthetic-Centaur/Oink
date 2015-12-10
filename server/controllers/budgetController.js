7e8f864dc9e05682fc6555f1e2f4b3562ead9eb1
tlevin <toddlevin802@gmail.com>
2015-12-09 16:00:50 -0800

    Saving data to budget table properly

--- server/controllers/budgetController.js
+++ server/controllers/budgetController.js
@@ -1,23 +1,27 @@
 import Budget from '../db/models/budget'
+import Category from '../db/models/category'
 
 let budgetController = {
-  createBudget(categoryId, userId, amount){
-    let newBudget = {
-      amount,
-      category_id: categoryId,
-      user_id: userId
-    }
-    //sum all transactions, set to variable actual
-    let actual = 0
-    newBudget.actual = actual
-    //save to table
-    newBudget = new Budget(newBudget)
-    return newBudget.save().then((budget) => {
-      if(budget){
-        return budget
-      } else {
-        return null
+  createBudget(category, userId, amount){
+    let searchCat = new Category({ description: category })
+    return searchCat.fetch().then( (cat) => {
+      let newBudget = {
+        target: amount,
+        category_id: cat.id,
+        user_id: userId
       }
+      //sum all transactions, set to variable actual
+      let actual = 0
+      newBudget.actual = actual
+      //save to table
+      newBudget = new Budget(newBudget)
+      return newBudget.save().then((budget) => {
+        if(budget){
+          return budget
+        } else {
+          return null
+        }
+      })
     })
     //return budget
   },
