dd0eb4ad3cb2d817bf81dd55600396f4688a2633
tlevin <toddlevin802@gmail.com>
2015-12-09 19:19:18 -0800

    Recreate temp files

---
+++ client/config/authKeys-temp.js
@@ -0,0 +1,15 @@
+/******************************************************
+TODO:
+1) Create a new file called authKeys.js in this folder
+2) Copy this file into authKeys.js, and replace the
+   the necessary keys
+
+Note: authKeys.js will always be git ignored
+******************************************************/
+let authKeys = {
+  plaid: {
+    public_key: '6495117a9cdcf1a32296c340eaf447'
+  }
+}
+
+export default authKeys
