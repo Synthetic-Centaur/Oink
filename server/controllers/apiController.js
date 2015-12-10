313af276681a65708a253ba27f05a530fe6d01f0
Aaron Ackerman <a.ackerman050@gmail.com>
2015-12-09 17:54:28 -0800

    Building out budgetController

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -2,8 +2,7 @@ import User from '../db/models/user'
 import Users from '../db/collections/users'
 import Category from '../db/models/category'
 import Categories from '../db/collections/categories'
-
-
+import budgetController from './budgetController'
 // private environmental variables
 import config from '../env/envConfig'
 
@@ -27,8 +26,8 @@ import plaid from 'plaid'
 import bluebird from 'bluebird'
 bluebird.promisifyAll(plaid)
 
-let plaidClient = new plaid.Client(clientId, secret, plaid.environments.tartan)
-//let plaidClient = new plaid.Client('test_id', 'test_secret', plaid.environments.tartan)
+// let plaidClient = new plaid.Client(clientId, secret, plaid.environments.tartan)
+let plaidClient = new plaid.Client('test_id', 'test_secret', plaid.environments.tartan)
 
 
 
@@ -37,10 +36,11 @@ let apiController = {
   tradeToken(public_token) {
     return plaidClient.exchangeTokenAsync(public_token)
   },
-  getTransactions(plaid_token) {
-    plaidClient.getConnectUser(plaid_token,
+  getTransactions(plaid_token, userid) {
+    console.log('palid_token: ', plaid_token)
+    return plaidClient.getConnectUser('test_wells',
     {
-      gte: '30 days ago',
+      gte: '360 days ago',
       // TODO: update webhook
       webhook: 'http://a2ec5c23.ngrok.io'
     },
@@ -48,9 +48,71 @@ let apiController = {
       if (err) {
         console.log('ERROR', err);
       } else {
-        console.log('You have ' + response.transactions.length +' transactions from the last thirty days.');
+        /////////////////////For testing purposes, please remove///////////////////
+
+        const transactions = [{
+          "_account": "XARE85EJqKsjxLp6XR8ocg8VakrkXpTXmRdOo",
+          "_id": "0AZ0De04KqsreDgVwM1RSRYjyd8yXxSDQ8Zxn",
+          "amount": 200,
+          "date": "2014-07-21",
+          "name": "ATM Withdrawal",
+          "meta": {
+            "location": {
+              "city": "San Francisco",
+              "state": "CA"
+            }
+          },
+          "pending": false,
+          "type": {
+            "primary": "special"
+          },
+          "category": [
+            "Transfer",
+            "Withdrawal",
+            "ATM"
+          ],
+          "category_id": "21012002",
+          "score": {
+            "location": {
+              "city": 1,
+              "state": 1
+            },
+            "name": 1
+          }
+        },
+        {
+          "_account": "XARE85EJqKsjxLp6XR8ocg8VakrkXpTXmRdOo",
+          "_id": "3mg4qV4JZycjewvKEzrLTYMzdr1MmvcO4Z3zX",
+          "amount": 240,
+          "date": "2014-07-24",
+          "name": "Online Transfer from External Sav ...3092",
+          "meta": {
+            "location": {}
+          },
+          "pending": false,
+          "type": {
+            "primary": "special"
+          },
+          "category": [
+            "Transfer",
+            "Account Transfer"
+          ],
+          "category_id": "21001000",
+          "score": {
+            "location": {},
+            "name": 1
+          }
+        }]
+
+
+        ///////////////////////////////////////////////
+        console.log("transactions: ", transactions)
+        // console.log('Transactions: ', response.transactions);
+        // console.log('You have ' + response.transactions.length +' transactions from the last 400 days.');
         // TODO: need to make async and move this logic to controller to send back
+        budgetController.saveTransactions(transactions, userid)
         //res.send(response.transactions);
+        //call saveTransactions controller to save transactions into database: budgetController.saveTransactions(transactions)
       }
     })
   },
