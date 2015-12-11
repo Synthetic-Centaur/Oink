abc9bdb387de9d9bc104bc25f302c1da0f63fde8
Clayton Schneider <claykschneider@gmail.com>
2015-12-10 19:02:54 -0800

    add 0.0.0.0 for host when prod env

--- server/server.js
+++ server/server.js
@@ -6,12 +6,22 @@ var app = new Express()
 
 const port = process.env.PORT || 3000
 
-app.listen(port, (error) => {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info(`==>   Listening on port ${port}. NODE_ENV is ${process.env.NODE_ENV}.`)
-  }
-})
+if (process.env.NODE_ENV === 'dev') {
+  app.listen(port, (error) => {
+    if (error) {
+      console.error(error)
+    } else {
+      console.info(`==>   Listening on port ${port}. NODE_ENV is ${process.env.NODE_ENV}.`)
+    }
+  })
+} else {
+  app.listen(port, '0.0.0.0', (error) => {
+    if (error) {
+      console.error(error)
+    } else {
+      console.info(`==>   Listening on port ${port}. NODE_ENV is ${process.env.NODE_ENV}.`)
+    }
+  })
+}
 
 export default app
