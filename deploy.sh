674292be812bb2b3dc7ab0a2ae823711d9ccc367
Clayton Schneider <claykschneider@gmail.com>
2015-12-10 17:32:48 -0800

    update deploy with env name and escape space

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ aws configure set default.region us-west-1
 aws configure set default.output json
 
 SHA1=$1
-ENVNAME=$2
 
 # Deploy image to Docker Hub
 docker push oink/oink-financial:$SHA1
@@ -15,9 +14,9 @@ EB_BUCKET=elasticbeanstalk-us-west-1-591231839583
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name oink \
+aws elasticbeanstalk create-application-version --application-name Oink\ Financial \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name $ENVNAME \
+aws elasticbeanstalk update-environment --environment-name oink \
     --version-label $SHA1
