ebf5859a1fe2e6eadefc5c4d75ee2cc3549e834b
Clayton Schneider <claykschneider@gmail.com>
2015-12-10 19:40:00 -0800

    Update elastic beanstalk to point to new application instance

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,9 @@ EB_BUCKET=elasticbeanstalk-us-west-1-591231839583
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name Oink\ Financial \
+aws elasticbeanstalk create-application-version --application-name oink-financial \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name oink \
+aws elasticbeanstalk update-environment --environment-name oinkfinancial-env \
     --version-label $SHA1
