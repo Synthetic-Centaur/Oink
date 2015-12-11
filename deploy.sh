db4012bc642e6d7ae37b17261bd40af86cac5278
Clayton Schneider <claykschneider@gmail.com>
2015-12-10 16:56:07 -0800

    modified bash script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # deploy.sh
 #! /bin/bash
 
-aws configure set default.region us-west-2
+aws configure set default.region us-west-1
 aws configure set default.output json
 
 SHA1=$1
@@ -11,11 +11,11 @@ ENVNAME=$2
 docker push oink/oink-financial:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=elasticbeanstalk-us-west-2-344528048408
+EB_BUCKET=elasticbeanstalk-us-west-1-591231839583
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name Oink-Financial \
+aws elasticbeanstalk create-application-version --application-name oink \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
