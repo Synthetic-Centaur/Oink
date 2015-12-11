70e025355741a9e2bdbdcfa5c3c0ca6eba6f7144
tlevin <toddlevin802@gmail.com>
2015-12-11 08:47:49 -0800

    Fixed issue if user not found would still check for password

--- server/handlers/authHandler.js
+++ server/handlers/authHandler.js
@@ -50,28 +50,29 @@ let authHandler = {
       if (!user) {
         res.status(403)
         res.json({ success: false, message: 'Authentication failed. User not found.' })
-      }
-      // Checks if provided password is valid
-      if (!user.validPassword(req.body.password)) {
-        res.status(403)
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' })
-      }
-      // if user is found and password is right
-      // create a token
-      let token = jwt.sign(user, 'jwt_secret', {
-        expiresIn: 604800 // expires in 24 hours
-      })
-
-      authController.saveAuthToken(token, user.attributes.id).then( (user) => {    
-        // return the information including token as JSON
-        res.status(200)
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          jwt_token: token,
-          expiresIn: 604800
+      } else {
+        // Checks if provided password is valid
+        if (!user.validPassword(req.body.password)) {
+          res.status(403)
+          res.json({ success: false, message: 'Authentication failed. Wrong password.' })
+        }
+        // if user is found and password is right
+        // create a token
+        let token = jwt.sign(user, 'jwt_secret', {
+          expiresIn: 604800 // expires in 24 hours
         })
-      })
+
+        authController.saveAuthToken(token, user.attributes.id).then( (user) => {    
+          // return the information including token as JSON
+          res.status(200)
+          res.json({
+            success: true,
+            message: 'Enjoy your token!',
+            jwt_token: token,
+            expiresIn: 604800
+          })
+        })
+      }
     })
   },
   signup(req, res, next) {
--- test/server/routeTest.js
+++ test/server/routeTest.js
@@ -1,41 +1,47 @@
-import request from 'supertest'
 let expect = require('chai').expect
 process.env.NODE_ENV = 'dev'
 var app = require('../../server/index')
 var server = require('../../server/server').server
+var request = require('supertest')(server)
 
 describe('Static routing', () => {
   it('should allow root requests to /', (done) => {
-    request(server)
+    request
       .get('/')
       .expect(200, done)
   })
   it('should respond with 404 on routes not present in static file', (done) => {
-    request(server)
+    request
       .get('/notinserver')
       .expect(404, done)
   })
 })
 describe('AuthRoutes', () => {
   it('should respond with a 403 forbidden with unauthorized request to auth/plaid', (done) => {
-    request(server)
+    request
       .get('/auth/plaid')
       .expect(403, done)
   })
   it('should respond with a 403 forbidden with unauthorized request to auth/login', (done) => {
-    request(server)
+    var invalidUser = {
+      email: "jim@example.com",
+      password: "KittyKat"
+    }
+
+    request
       .post('/auth/login')
+      .send(invalidUser)
       .expect(403, done)
   })
 })
 describe('APIRoutes', () => {
   it('should respond with a 403 forbidden with unauthorized request to /api/initialState', (done) => {
-    request(server)
+    request
       .get('/api/initialState')
       .expect(403, done)
   })
   it('should respond with a 403 forbidden with unauthorized request to /api/budget/category/:id', (done) => {
-    request(server)
+    request
       .post('/api/budget/category/Transfer')
       .expect(403, done)
   })
