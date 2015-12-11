064fcaaee2462e493e5aec6f0af970049ef5effd
Clayton Schneider <claykschneider@gmail.com>
2015-12-10 16:00:06 -0800

    Comment out tests for circle integration

--- test/unit/ExampleTest.js
+++ test/unit/ExampleTest.js
@@ -1,12 +1,15 @@
 
+
 // describe('the todo.App', function() {
 //   describe('the todo object', function(){
 //     it('should be an object', function () {
-//       expect(apiHandler).to.be.a('Object');
+//       expect(todo).to.be.a('Object');
 //     });
 //     it('should have all the necessary methods', function(){
-//       expect(apiHandler.initialState).to.be.a('Function');
-//       expect(apiHandler.budget).to.be.a('Function');
+//       expect(todo.util.trimTodoName).to.be.a('Function');
+//       (todo.util.isValidTodoName).should.to.be.a('Function');
+//       assert.typeOf(todo.util.getUniqueId, 'Function');
 //     });
 //   });
 // });
+
