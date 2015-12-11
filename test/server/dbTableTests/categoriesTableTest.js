6e5c2c5813b9a725422477239aa8445aa930f8d8
tlevin <toddlevin802@gmail.com>
2015-12-11 13:57:04 -0800

    Categories table test complete

--- test/server/dbTableTests/categoriesTableTest.js
+++ test/server/dbTableTests/categoriesTableTest.js
@@ -3,7 +3,7 @@ import db from '../../../server/db/dbConfig'
 import util from '../util'
 
 
-describe('Budget Table', () => {
+describe('Category Table', () => {
   before( (done) => {
     util.clearDB().then(() => {
       util.populateDB( () => {
@@ -17,36 +17,13 @@ describe('Budget Table', () => {
     })
   })
   
-  it('should have a user_id column which is an integer', (done) => {
-    db.knex('budgets').columnInfo('user_id').then((info) => {
-      expect(info.type).to.equal('integer')
+  it('should have a description column which is a varchar', (done) => {
+    db.knex('categories').columnInfo('description').then((info) => {
+      expect(info.type).to.equal('character varying')
       done()
     })
   })
-  it('should have a Budget\'s table', (done) => {
-    db.knex.schema.hasTable('budgets').then( (exists) => {
-      expect(exists).to.equal(true)
-      done()
-    })
-  })
-  it('should have a category_id column which is an integer', (done) => {
-    db.knex('budgets').columnInfo('category_id').then((info) => {
-      expect(info.type).to.equal('integer')
-      done()
-    })
-  })
-  it('should have a target column which is an integer', (done) => {
-    db.knex('budgets').columnInfo('target').then((info) => {
-      expect(info.type).to.equal('integer')
-      done()
-    })
-  })
-  it('should have a actual column which is a decimal', (done) => {
-    db.knex('budgets').columnInfo('user_id').then((info) => {
-      expect(info.type).to.equal('integer')
-      done()
-    })
-  })
+  
 
 })
 
