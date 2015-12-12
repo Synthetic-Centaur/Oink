e86fa21b120b3ae555662fcc60c4390c9e50afac
tlevin <toddlevin802@gmail.com>
2015-12-12 08:49:49 -0800

    Goal Table testing

--- test/server/dbTableTests/categoriesTableTest.js
+++ test/server/dbTableTests/categoriesTableTest.js
@@ -1,21 +1,8 @@
   let expect = require('chai').expect
 import db from '../../../server/db/dbConfig'
-import util from '../util'
 
 
 describe('Category Table', () => {
-  beforeEach( (done) => {
-    util.clearDB().then(() => {
-      util.populateDB( () => {
-        setTimeout(done, 250)
-      })
-    })
-  })
-  after( (done) => {
-    util.populateDB( () => {
-        done()
-    })
-  })
   
   it('should have a description column which is a varchar', (done) => {
     db.knex('categories').columnInfo('description').then((info) => {
