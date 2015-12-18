592cce02503cecb0d9901def60c4ebebe04f0412
Clayton Schneider <claykschneider@gmail.com>
2015-12-18 14:41:18 -0800

    Create enviroment variable on window to disable devtools in production. Add webpack minification parameters for production.

--- server/middleware.js
+++ server/middleware.js
@@ -5,26 +5,26 @@ import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
 
-// Webpack dependencies
-import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-import webpackConfig from '../webpack.config'
-
 import db from './db/dbConfig.js'
 import {populateTables} from './db/dbConfig'
 populateTables(() => {})
 
 import app from './server'
 
-const compiler = webpack(webpackConfig)
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
 
-if (process.env.NODE_ENV === 'dev') {
+  // Webpack dependencies
+  let webpack = require('webpack')
+  let webpackDevMiddleware = require('webpack-dev-middleware')
+  let webpackHotMiddleware = require('webpack-hot-middleware')
+  let webpackConfig = require('../webpack.config')
+
+  let compiler = webpack(webpackConfig)
+  
   // Webpack compiling for hot reloads
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }))
+  app.use(webpackHotMiddleware(compiler))
 }
-
-app.use(webpackHotMiddleware(compiler))
 
 // Body Parser middleware for handling requests
 app.use(bodyParser.urlencoded({ extended: false }))
