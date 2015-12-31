39ffee19928d2b870126408a9560e70dcd792842
Clayton Schneider <claykschneider@gmail.com>
2015-12-30 18:13:52 -0800

    Fix reducers to have phone verify error

--- client/reducers/reducers.js
+++ client/reducers/reducers.js
@@ -1,10 +1,9 @@
+
 /***************************************************************************************
 These are the reducers that manage changes to the state of our application.
-
 Each reducer takes in the current piece (a key on state defined in combineReducers)
 of state that it is responsible for managing. We provide default values for each of
 these pieces of state.
-
 The signature for a reducer is (state, action) => return state
 A reducer will return a new state, using Object.assign, depending on what actions was
 dispatched as the result of user interaction. If there is no relevant action, it will
@@ -14,10 +13,6 @@ these actions.
 
 import { DROPDOWN_ACTIONS } from '../constants/componentActions'
 
-// ## **DASHBOARD REDUCERS** ##
-
-// ### CURRENT COMPONENT REDUCER ###
-// Receives selected component from Nav Bar and changes the component on state
 export function dashboardReducer(state = {
   currentComponent: DROPDOWN_ACTIONS[0]
 }, action = null) {
@@ -29,8 +24,6 @@ export function dashboardReducer(state = {
   }
 }
 
-// ### BUDGET COMPONENT REDUCER ###
-// Sets data from Budget component on state
 export function budgetReducer(state = {
   settingsView: 'ADD',
   currentBudget: { index: null }
@@ -45,8 +38,86 @@ export function budgetReducer(state = {
   }
 }
 
-// ### BUDGET, SETTINGS, AND PHONE REDUCERS ###
-// Receives validation data from budgets and pop-up data from settings and phone and stores them on state
+export function plaidReducer(state = {
+  publicKey: '',
+  showPlaid: false
+}, action=null) {
+  switch (action.type) {
+    case 'ADD_PLAID_KEY':
+      return Object.assign({}, state, {publicKey: action.data})
+    case 'SHOW_PLAID':
+      return Object.assign({}, state, {showPlaid: true})
+    case 'HIDE_PLAID':
+      return Object.assign({}, state, {showPlaid: false})
+    default:
+      return state
+  }
+}
+
+export function asyncStatusReducer(state = {
+  isLoading: false,
+  data: {},
+  error: false
+}, action = null) {
+  switch (action.type) {
+
+    case 'RECV_ERROR':
+      return Object.assign({}, state, {isLoading: false, data: action.data, error: true})
+    case 'RECV_DATA':
+      return Object.assign({}, state, {isLoading: false, data: action.data, error: false})
+    case 'REQ_DATA':
+      return Object.assign({}, state, {isLoading: true, error: false})
+    case 'REQ_FINISHED':
+      return Object.assign({}, state, {isLoading: false, error: false})
+    default:
+      return state
+  }
+}
+
+export function splashPageReducer(state = {
+  showLogin: false,
+  showSignup: false,
+  invalidEmail: false,
+  invalidPassword: false,
+  invalidPhone: false,
+  invalidBank: false,
+  userExists: false,
+  passwordMatchError: false,
+  missingSignupFields: false,
+  errorText: ''
+}, action = null) {
+  switch (action.type) {
+
+    case 'SHOW_LOGIN':
+      return Object.assign({}, state, {showLogin: true})
+    case 'HIDE_LOGIN':
+      return Object.assign({}, state, {showLogin: false})
+    case 'SHOW_SIGNUP':
+      return Object.assign({}, state, {showSignup: true})
+    case 'HIDE_SIGNUP':
+      return Object.assign({}, state, {showSignup: false})
+    case 'REMOVE_ALERTS':
+      return Object.assign({}, state, {invalidBank: false, invalidEmail: false, userExists: false, invalidPassword: false, errorText: ''})
+    case 'INVALID_EMAIL':
+      return Object.assign({}, state, {invalidEmail: true, errorText: 'Oops, looks like that email doesn\'t exist! Please sign up to get started.'})
+    case 'INVALID_PASSWORD':
+      return Object.assign({}, state, {invalidPassword: true, errorText: 'Oops, looks like that password isn\'t right!'})
+    case 'INVALID_BANK':
+      return Object.assign({}, state, {invalidBank: true, errorText: 'Looks like you haven\'t authorized your bank with us yet.'})
+    case 'USER_EXISTS':
+      return Object.assign({}, state, {userExists: true, errorText: 'Looks like there is already a user with that email in our system. Please choose a different email.'})
+    case 'PASSWORD_MATCH_ERR':
+      return Object.assign({}, state, {passwordMatchError: true, errorText: 'Oops, looks like those passwords don\'t match. Please re-enter your password in both fields and try again.'})
+    case 'PHONE_NUMBER_ERR':
+      return Object.assign({}, state, {invalidPhone: true, errorText: 'Please enter a 10 digit phone number.'})
+    case 'MISSING_SIGNUP_FIELDS':
+      return Object.assign({}, state, {missingSignupFields: true, errorText: 'Please fill out all fields.'})
+    default:
+      return state
+  }
+}
+
+//Change state to allow or disallow form input
 export function homePageReducer(state = {
   numberError: true,
   categoryError: true,
@@ -89,8 +160,6 @@ export function homePageReducer(state = {
   }
 }
 
-// ### SPENDING COMPONENT REDUCER ###
-//Sets data from spending page component on state
 export function spendingPageReducer(state = {
   selectedDate: null
 }, action = null) {
@@ -102,8 +171,6 @@ export function spendingPageReducer(state = {
   }
 }
 
-// ### SETTINGS COMPONENT REDUCER ###
-//Sets data from settings component on state
 export function settingsReducer(state = {
   editingFirstName: false,
   editingLastName: false,
@@ -171,8 +238,6 @@ export function settingsReducer(state = {
   }
 }
 
-// ### GOAL COMPONENT REDUCER ###
-//Sets data from Goal component on state
 export function goalPageReducer(state = {
   selectedGoal: 1,
   isValid: false,
@@ -194,8 +259,6 @@ export function goalPageReducer(state = {
   }
 }
 
-// ### TRANSACTION MAP COMPONENT REDUCER ###
-//Sets data from Trasaction Map component on state
 export function transactionMapReducer(state = {
   childrenCluster: {},
   mapDate: {},
@@ -211,94 +274,6 @@ export function transactionMapReducer(state = {
   }
 }
 
-// ## PLAID REDUCER
-
-// Receives state information dictating whether or not to show plaid modal and sets plaid key on state
-export function plaidReducer(state = {
-  publicKey: '',
-  showPlaid: false
-}, action=null) {
-  switch (action.type) {
-    case 'ADD_PLAID_KEY':
-      return Object.assign({}, state, {publicKey: action.data})
-    case 'SHOW_PLAID':
-      return Object.assign({}, state, {showPlaid: true})
-    case 'HIDE_PLAID':
-      return Object.assign({}, state, {showPlaid: false})
-    default:
-      return state
-  }
-}
-
-// ## ASYNCHRONOUS REDUCER
-
-// Receives data from asynchronous calls to the server and sets data on state
-
-export function asyncStatusReducer(state = {
-  isLoading: false,
-  data: {},
-  error: false
-}, action = null) {
-  switch (action.type) {
-
-    case 'RECV_ERROR':
-      return Object.assign({}, state, {isLoading: false, data: action.data, error: true})
-    case 'RECV_DATA':
-      return Object.assign({}, state, {isLoading: false, data: action.data, error: false})
-    case 'REQ_DATA':
-      return Object.assign({}, state, {isLoading: true, error: false})
-    default:
-      return state
-  }
-}
-
-// ## SPLASH PAGE REDUCER
-
-// Receives data regarding validation and pop-up modals
-
-export function splashPageReducer(state = {
-  showLogin: false,
-  showSignup: false,
-  invalidEmail: false,
-  invalidPassword: false,
-  invalidBank: false,
-  userExists: false,
-  passwordMatchError: false,
-  missingSignupFields: false,
-  errorText: ''
-}, action = null) {
-  switch (action.type) {
-
-    case 'SHOW_LOGIN':
-      return Object.assign({}, state, {showLogin: true})
-    case 'HIDE_LOGIN':
-      return Object.assign({}, state, {showLogin: false})
-    case 'SHOW_SIGNUP':
-      return Object.assign({}, state, {showSignup: true})
-    case 'HIDE_SIGNUP':
-      return Object.assign({}, state, {showSignup: false})
-    case 'REMOVE_ALERTS':
-      return Object.assign({}, state, {invalidBank: false, invalidEmail: false, userExists: false, invalidPassword: false, errorText: ''})
-    case 'INVALID_EMAIL':
-      return Object.assign({}, state, {invalidEmail: true, errorText: 'Oops, looks like that email doesn\'t exist! Please sign up to get started.'})
-    case 'INVALID_PASSWORD':
-      return Object.assign({}, state, {invalidPassword: true, errorText: 'Oops, looks like that password isn\'t right!'})
-    case 'INVALID_BANK':
-      return Object.assign({}, state, {invalidBank: true, errorText: 'Looks like you haven\'t authorized your bank with us yet.'})
-    case 'USER_EXISTS':
-      return Object.assign({}, state, {userExists: true, errorText: 'Looks like there is already a user with that email in our system. Please choose a different email.'})
-    case 'PASSWORD_MATCH_ERR':
-      return Object.assign({}, state, {passwordMatchError: true, errorText: 'Oops, looks like those passwords don\'t match. Please re-enter your password in both fields and try again.'})
-    case 'MISSING_SIGNUP_FIELDS':
-      return Object.assign({}, state, {missingSignupFields: true, errorText: 'Please fill out all fields.'})
-    default:
-      return state
-  }
-}
-
-// ## AUTHENTICATION REDUCER
-
-// Sets user access token and validation on state
 export function authReducer(state = {
   isAuthenticated: false,
   token: '',
