49c0c54905f1abdd1d5316a9c7f5aa96ca3482e2
Aaron Ackerman <a.ackerman050@gmail.com>
2015-12-31 11:40:53 -0800

    Finishing docs

--- tableofcontents.js
+++ tableofcontents.js
@@ -1,244 +1,128 @@
 // # Table of Contents
 
 
-// ## Front-End
+// # Front-End
+
+  // ### CLIENT-SIDE SETUP
+      // * [index.js](./client!index.js.html) - (React/Redux Application entry point)
+      // * [configureStore.js](./client!store!configureStore.js.html) - (Configuration for redux store)
+      // * [chart-theme.js](./client!chart-theme.js.html) - (General theme for High Charts styling)
+      // * [material-theme.js](./client!material-theme.js.html) - (Material-UI styles theme)
+      // * [splash-theme.js](./client!splash-theme.js.html) - (Styling and themes for splash page)
+
+
+  // ### ACTIONS
+  // * [actions.js](./client!actions!actions.js.html) -  (Actions for sending component data to store)
+  // * [apiActions.js](./client!actions!api!apiActions.js.html) - (Actions for server-side user data)
+  // * [authActions.js](./client!actions!api!authActions.js.html) - (Actions for server authentication data)
+
+  // ### COMPONENTS
+    // #### Budget Component
+        // * [BarChart.js](./client!components!dashboard!budget!BarChart.js.html) - (Transaction data vs budget data)
+        // * [barChartConfig.js](./client!components!dashboard!budget!config!barChartConfig.js.html) - (High-Charts configuration)
+        // * [BudgetCategories.js](./client!components!dashboard!budget!BudgetCategories.js.html) - (Menu for submitting new budgets)
+        // * [BudgetList.js](./client!components!dashboard!budget!BudgetList.js.html) -  (List for displaying current budgets)
+        // * [OptionsBar.js](./client!components!dashboard!budget!OptionsBar.js.html) - (Budget options)
+        // * [PieChart.js](./client!components!dashboard!budget!PieChart.js.html) - (Pie chart for displaying budgets)
+        // * [chartConfig.js](./client!components!dashboard!budget!config!chartConfig.js.html) - (High-Charts configuration)
+        // * [UpdateField.js](./client!components!dashboard!budget!UpdateField.js.html) - (Submission field for budgets)
+        // * [WelcomeMessage.js](./client!components!dashboard!budget!WelcomeMessage.js.html) - (Budget Welcome message)
+    // ***
+    // #### Goal Component
+        // * [GoalAdd.js](./client!components!dashboard!goals!GoalAdd.js.html) - (Submission form for new goal)
+        // * [GoalUpdate.js](./client!components!dashboard!goals!GoalUpdate.js.html) - (Submission form for updating goal)
+        // * [GoalConfigurer.js](./client!components!dashboard!goals!GoalConfigurer.js.html) - (Goal configuration)
+        // * [GoalChart.js](./client!components!dashboard!goals!GoalChart.js.html) - (Chart of current goals)
+        // * [chartConfig.js](./client!components!dashboard!goals!config!chartConfig.js.html) - (High-Charts configuration)
+        // * [GoalList.js](./client!components!dashboard!goals!GoalList.js.html) - (List of Current goals)
+        // * [MessageCenter.js](./client!components!dashboard!goals!MessageCenter.js.html) - (Default goal message)
+    // ***
+    // #### Map Component
+        // * [TransactionMap.js](./client!components!dashboard!heatmap!TransactionMap.js.html) - (Heat map for user data)
+    // ***
+    // #### Phone Verification
+        // * [PhoneVerifyModal.js](./client!components!dashboard!phoneVerify!PhoneVerifyModal.js.html) - (Pop-up for validating phone number)
+        // * [PhoneVerifyIcon.js](./client!components!dashboard!phoneVerify!PhoneVerifyIcon.js.html) - (Phone validation pop-up icon)
+    // ***
+    // #### Settings
+        // * [SettingsModal.js](./client!components!dashboard!settings!SettingsModal.js.html) - (Settings Pop-up)
+        // * [AccountSettingsField.js](./client!components!dashboard!settings!AccountSettingsField.js.html) - (General account settings)
+        // * [CommunicationSettingsField.js](./client!components!dashboard!settings!CommunicationSettingsField.js.html) - (Email and phone settings)
+        // * [SecuritySettingsField.js](./client!components!dashboard!settings!SecuritySettingsField.js.html) - (Security settings)
+    // ***
+    // #### Side Navigation 
+        // * [SideNav.js](./client!components!dashboard!sidenav!SideNav.js.html) - (Feature navigation)
+    // ***
+    // #### Spending
+        // * [SpendingChart.js](./client!components!dashboard!spending!SpendingChart.js.html) - (Transaction chart)
+        // * [DateTransactions.js](./client!components!dashboard!spending!DateTransactions.js.html) - (Transaction list for selected date)
+        // * [InfoModule.js](./client!components!dashboard!spending!InfoModule.js.html) - (Spending component information)
+        // * [chartConfig.js](./client!components!dashboard!spending!config!chartConfig.js.html) - (High-Charts configuration)
+    // ***
+    // #### Acount modal
+        // * [AccountModal.js](./client!components!splash!account-modal!AccountModal.js.html)
+        // * [LoginField.js](./client!components!splash!account-modal!LoginField.js.html) - (Login modal)
+        // * [SignupField.js](./client!components!splash!account-modal!SignupField.js.html) - (Signup Modal)
+        // * [PlaidButton.js](./client!components!splash!account-modal!PlaidButton.js.html) - (Plaid modal)
+    // ***
+    // #### Splash
+        // * [SplashNavBar.js](./client!components!splash!navbar!SplashNavBar.js.html) - (Splash page navigation)
+        // * [ProfileCard.js](./client!components!splash!ProfileCard.js.html)
+    // ***
+    // #### Component Setup
+        // * [Options.js](./client!components!dashboard!Options.js.html) - (Settings and logout)
+        // * [componentActions.js](./client!constants!componentActions.js.html) - (Component object for nav-bar)
+        // * [LoadingIndicator.js](./client!components!dashboard!LoadingIndicator.js.html) - (Load bar setup)
+
+  // ### CONTAINERS
+  // #### (Containers for passing data and actions into components)
+      // * [Budget.js](./client!containers!Budget.js.html)
+      // * [ComponentPlayground.js](./client!containers!ComponentPlayground.js.html)
+      // * [Dashboard.js](./client!containers!Dashboard.js.html)
+      // * [Goals.js](./client!containers!Goals.js.html)
+      // * [Map.js](./client!containers!Map.js.html)
+      // * [Spending.js](./client!containers!Spending.js.html)
+      // * [Splash.js](./client!containers!Splash.js.html)
+
+  // ### REDUCERS
+  // #### (Reducers for changing data on state)
+    // * [index.js](./client!reducers!index.js.html)
+    // * [reducers.js](./client!reducers!reducers.js.html)
+
+// # Back-End
+
+
+  // ### SERVER-SIDE SETUP
+      // * [server.js](./server!server.js.html)
+      // * [envConfig-temp.js](./server!env!envConfig-temp.js.html)
+      // * [middleware.js](./server!middleware.js.html)
+      // * [routes.js](./server!routes!routes.js.html)
+      // * [schedule.js](./server!schedule.js.html)
+
+  // ### CONTROLLERS
+  // * [apiController.js](./server!controllers!apiController.js.html)
+  // * [authController.js](./server!controllers!authController.js.html)
+  // * [budgetController.js](./server!controllers!budgetController.js.html)
+  // * [cronController.js](./server!controllers!cronController.js.html)
+  // * [goalController.js](./server!controllers!goalController.js.html)
+  // * [transactionController.js](./server!controllers!transactionController.js.html)
+
+  // ### DATABASE
+  // * [budgets.js](./server!db!collections!budgets.js.html)
+  // * [categories.js](./server!db!collections!categories.js.html)
+  // * [transactions.js](./server!db!collections!transactions.js.html)
+  // * [users.js](./server!db!collections!users.js.html)
+  // * [dbConfig.js](./server!db!dbConfig.js.html)
+  // * [budget.js](./server!db!models!budget.js.html)
+  // * [category.js](./server!db!models!category.js.html)
+  // * [transaction.js](./server!db!models!transaction.js.html)
+  // * [user.js](./server!db!models!user.js.html)
+
+
+  // ### HANDLERS
+  // * [apiHandler.js](./server!handlers!apiHandler.js.html)
+  // * [authHandler.js](./server!handlers!authHandler.js.html)
+  // * [cronHandler.js](./server!handlers!cronHandler.js.html)
 
-// #### [index.js](./client!index.js.html)
-// * React/Redux Application entry point
 
-// #### [chart-theme.js](./client!chart-theme.js.html)
-// * General theme for High Charts styling
-
-// #### [material-theme.js](./client!material-theme.js.html)
-// * Material-UI styles theme
-
-// ### ACTIONS
-
-// #### [actions.js](./client!actions!actions.js.html)
-// * React actions
-
-// #### [apiActions.js](./client!actions!api!apiActions.js.html)
-// * React api actions
-
-// #### [authActions.js](./client!actions!api!authActions.js.html)
-// * React api actions
-
-// ### COMPONENTS
-
-// #### [BarChart.js](./client!components!dashboard!budget!BarChart.js.html)
-// * React api actions
-
-// #### [BudgetCategories.js](./client!components!dashboard!budget!BudgetCategories.js.html)
-// * React api actions
-
-// #### [BudgetList.js](./client!components!dashboard!budget!BudgetList.js.html)
-// * React api actions
-
-// #### [barChartConfig.js](./client!components!dashboard!budget!config!barChartConfig.js.html)
-// * React api actions
-
-// #### [chartConfig.js](./client!components!dashboard!budget!config!chartConfig.js.html)
-// * React api actions
-
-// #### [OptionsBar.js](./client!components!dashboard!budget!OptionsBar.js.html)
-// * React api actions
-
-// #### [PieChart.js](./client!components!dashboard!budget!PieChart.js.html)
-// * React api actions
-
-// #### [UpdateField.js](./client!components!dashboard!budget!UpdateField.js.html)
-// * React api actions
-
-// #### [WelcomeMessage.js](./client!components!dashboard!budget!WelcomeMessage.js.html)
-// * React api actions
-
-// #### [chartConfig.js](./client!components!dashboard!goals!config!chartConfig.js.html)
-// * React api actions
-
-// #### [GoalAdd.js](./client!components!dashboard!goals!GoalAdd.js.html)
-// * React api actions
-
-// #### [GoalChart.js](./client!components!dashboard!goals!GoalChart.js.html)
-// * React api actions
-
-// #### [GoalConfigurer.js](./client!components!dashboard!goals!GoalConfigurer.js.html)
-// * React api actions
-
-// #### [GoalList.js](./client!components!dashboard!goals!GoalList.js.html)
-// * React api actions
-
-// #### [GoalUpdate.js](./client!components!dashboard!goals!GoalUpdate.js.html)
-// * React api actions
-
-// #### [MessageCenter.js](./client!components!dashboard!goals!MessageCenter.js.html)
-// * React api actions
-
-// #### [TransactionMap.js](./client!components!dashboard!heatmap!TransactionMap.js.html)
-// * React api actions
-
-// #### [LoadingIndicator.js](./client!components!dashboard!LoadingIndicator.js.html)
-// * React api actions
-
-// #### [Options.js](./client!components!dashboard!Options.js.html)
-// * React api actions
-
-// #### [PhoneVerifyIcon.js](./client!components!dashboard!phoneVerify!PhoneVerifyIcon.js.html)
-// * React api actions
-
-// #### [PhoneVerifyModal.js](./client!components!dashboard!phoneVerify!PhoneVerifyModal.js.html)
-// * React api actions
-
-// #### [AccountSettingsField.js](./client!components!dashboard!settings!AccountSettingsField.js.html)
-// * React api actions
-
-// #### [CommunicationSettingsField.js](./client!components!dashboard!settings!CommunicationSettingsField.js.html)
-// * React api actions
-
-// #### [SecuritySettingsField.js](./client!components!dashboard!settings!SecuritySettingsField.js.html)
-// * React api actions
-
-// #### [SettingsModal.js](./client!components!dashboard!settings!SettingsModal.js.html)
-// * React api actions
-
-// #### [SideNav.js](./client!components!dashboard!sidenav!SideNav.js.html)
-// * React api actions
-
-// #### [chartConfig.js](./client!components!dashboard!spending!config!chartConfig.js.html)
-// * React api actions
-
-// #### [DateTransactions.js](./client!components!dashboard!spending!DateTransactions.js.html)
-// * React api actions
-
-// #### [InfoModule.js](./client!components!dashboard!spending!InfoModule.js.html)
-// * React api actions
-
-// #### [SpendingChart.js](./client!components!dashboard!spending!SpendingChart.js.html)
-// * React api actions
-
-// #### [AccountModal.js](./client!components!splash!account-modal!AccountModal.js.html)
-// * React api actions
-
-// #### [LoginField.js](./client!components!splash!account-modal!LoginField.js.html)
-// * React api actions
-
-// #### [PlaidButton.js](./client!components!splash!account-modal!PlaidButton.js.html)
-// * React api actions
-
-// #### [SignupField.js](./client!components!splash!account-modal!SignupField.js.html)
-// * React api actions
-
-// #### [SplashNavBar.js](./client!components!splash!navbar!SplashNavBar.js.html)
-// * React api actions
-
-// #### [ProfileCard.js](./client!components!splash!ProfileCard.js.html)
-// * React api actions
-
-// #### [componentActions.js](./client!constants!componentActions.js.html)
-// * React api actions
-
-// ### CONTAINERS
-
-// #### [Budget.js](./client!containers!Budget.js.html)
-// * React api actions
-
-// #### [ComponentPlayground.js](./client!containers!ComponentPlayground.js.html)
-// * React api actions
-
-// #### [Dashboard.js](./client!containers!Dashboard.js.html)
-// * React api actions
-
-// #### [Goals.js](./client!containers!Goals.js.html)
-// * React api actions
-
-// #### [Map.js](./client!containers!Map.js.html)
-// * React api actions
-
-// #### [Spending.js](./client!containers!Spending.js.html)
-// * React api actions
-
-// #### [Splash.js](./client!containers!Splash.js.html)
-// * React api actions
-
-// #### [index.js](./client!reducers!index.js.html)
-// * stuff
-
-// #### [reducers.js](./client!reducers!reducers.js.html)
-// * stuff
-
-// #### [splash-theme.js](./client!splash-theme.js.html)
-// * stuff
-
-// #### [configureStore.js](./client!store!configureStore.js.html)
-// * more stuff
-
-// ## Back-End
-
-// #### [server.js](./server!server.js.html)
-// * Node/Express Web Server
-
-// #### [apiController.js](./server!controllers!apiController.js.html)
-// * more stuff
-
-// #### [authController.js](./server!controllers!authController.js.html)
-// * more stuff
-
-// #### [budgetController.js](./server!controllers!budgetController.js.html)
-// * more stuff
-
-// #### [cronController.js](./server!controllers!cronController.js.html)
-// * more stuff
-
-// #### [goalController.js](./server!controllers!goalController.js.html)
-// * more stuff
-
-// #### [transactionController.js](./server!controllers!transactionController.js.html)
-// * more stuff
-
-// #### [budgets.js](./server!db!collections!budgets.js.html)
-// * more stuff
-
-// #### [categories.js](./server!db!collections!categories.js.html)
-// * more stuff
-
-// #### [transactions.js](./server!db!collections!transactions.js.html)
-// * more stuff
-
-// #### [users.js](./server!db!collections!users.js.html)
-// * more stuff
-
-// #### [dbConfig.js](./server!db!dbConfig.js.html)
-// * more stuff
-
-// #### [budget.js](./server!db!models!budget.js.html)
-// * more stuff
-
-// #### [category.js](./server!db!models!category.js.html)
-// * more stuff
-
-// #### [transaction.js](./server!db!models!transaction.js.html)
-// * more stuff
-
-// #### [user.js](./server!db!models!user.js.html)
-// * more stuff
-
-// #### [envConfig-temp.js](./server!env!envConfig-temp.js.html)
-// * more stuff
-
-// #### [apiHandler.js](./server!handlers!apiHandler.js.html)
-// * more stuff
-
-// #### [authHandler.js](./server!handlers!authHandler.js.html)
-// * more stuff
-
-// #### [cronHandler.js](./server!handlers!cronHandler.js.html)
-// * more stuff
-
-// #### [middleware.js](./server!middleware.js.html)
-// * more stuff
-
-// #### [routes.js](./server!routes!routes.js.html)
-// * more stuff
-
-// #### [schedule.js](./server!schedule.js.html)
-// * more stuff
 
