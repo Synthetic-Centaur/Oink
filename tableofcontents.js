642a673d4e694616a70f0abb2983c6d90500b20f
Aaron Ackerman <a.ackerman050@gmail.com>
2015-12-30 17:25:42 -0800

    Writing docco table of contents

--- tableofcontents.js
+++ tableofcontents.js
@@ -12,6 +12,152 @@
 // #### [material-theme.js](./client!material-theme.js.html)
 // * Material-UI styles theme
 
+// ### ACTIONS
+
+// #### [actions.js](./client!actions!actions.js.html)
+// * React actions
+
+// #### [apiActions.js](./client!actions!api!apiActions.js.html)
+// * React api actions
+
+// #### [authActions.js](./client!actions!api!authActions.js.html)
+// * React api actions
+
+// ### COMPONENTS
+
+// #### [BarChart.js](./client!components!dashboard!budget!BarChart.js.html)
+// * React api actions
+
+// #### [BudgetCategories.js](./client!components!dashboard!budget!BudgetCategories.js.html)
+// * React api actions
+
+// #### [BudgetList.js](./client!components!dashboard!budget!BudgetList.js.html)
+// * React api actions
+
+// #### [barChartConfig.js](./client!components!dashboard!budget!config!barChartConfig.js.html)
+// * React api actions
+
+// #### [chartConfig.js](./client!components!dashboard!budget!config!chartConfig.js.html)
+// * React api actions
+
+// #### [OptionsBar.js](./client!components!dashboard!budget!OptionsBar.js.html)
+// * React api actions
+
+// #### [PieChart.js](./client!components!dashboard!budget!PieChart.js.html)
+// * React api actions
+
+// #### [UpdateField.js](./client!components!dashboard!budget!UpdateField.js.html)
+// * React api actions
+
+// #### [WelcomeMessage.js](./client!components!dashboard!budget!WelcomeMessage.js.html)
+// * React api actions
+
+// #### [chartConfig.js](./client!components!dashboard!goals!config!chartConfig.js.html)
+// * React api actions
+
+// #### [GoalAdd.js](./client!components!dashboard!goals!GoalAdd.js.html)
+// * React api actions
+
+// #### [GoalChart.js](./client!components!dashboard!goals!GoalChart.js.html)
+// * React api actions
+
+// #### [GoalConfigurer.js](./client!components!dashboard!goals!GoalConfigurer.js.html)
+// * React api actions
+
+// #### [GoalList.js](./client!components!dashboard!goals!GoalList.js.html)
+// * React api actions
+
+// #### [GoalUpdate.js](./client!components!dashboard!goals!GoalUpdate.js.html)
+// * React api actions
+
+// #### [MessageCenter.js](./client!components!dashboard!goals!MessageCenter.js.html)
+// * React api actions
+
+// #### [TransactionMap.js](./client!components!dashboard!heatmap!TransactionMap.js.html)
+// * React api actions
+
+// #### [LoadingIndicator.js](./client!components!dashboard!LoadingIndicator.js.html)
+// * React api actions
+
+// #### [Options.js](./client!components!dashboard!Options.js.html)
+// * React api actions
+
+// #### [PhoneVerifyIcon.js](./client!components!dashboard!phoneVerify!PhoneVerifyIcon.js.html)
+// * React api actions
+
+// #### [PhoneVerifyModal.js](./client!components!dashboard!phoneVerify!PhoneVerifyModal.js.html)
+// * React api actions
+
+// #### [AccountSettingsField.js](./client!components!dashboard!settings!AccountSettingsField.js.html)
+// * React api actions
+
+// #### [CommunicationSettingsField.js](./client!components!dashboard!settings!CommunicationSettingsField.js.html)
+// * React api actions
+
+// #### [SecuritySettingsField.js](./client!components!dashboard!settings!SecuritySettingsField.js.html)
+// * React api actions
+
+// #### [SettingsModal.js](./client!components!dashboard!settings!SettingsModal.js.html)
+// * React api actions
+
+// #### [SideNav.js](./client!components!dashboard!sidenav!SideNav.js.html)
+// * React api actions
+
+// #### [chartConfig.js](./client!components!dashboard!spending!config!chartConfig.js.html)
+// * React api actions
+
+// #### [DateTransactions.js](./client!components!dashboard!spending!DateTransactions.js.html)
+// * React api actions
+
+// #### [InfoModule.js](./client!components!dashboard!spending!InfoModule.js.html)
+// * React api actions
+
+// #### [SpendingChart.js](./client!components!dashboard!spending!SpendingChart.js.html)
+// * React api actions
+
+// #### [AccountModal.js](./client!components!splash!account-modal!AccountModal.js.html)
+// * React api actions
+
+// #### [LoginField.js](./client!components!splash!account-modal!LoginField.js.html)
+// * React api actions
+
+// #### [PlaidButton.js](./client!components!splash!account-modal!PlaidButton.js.html)
+// * React api actions
+
+// #### [SignupField.js](./client!components!splash!account-modal!SignupField.js.html)
+// * React api actions
+
+// #### [SplashNavBar.js](./client!components!splash!navbar!SplashNavBar.js.html)
+// * React api actions
+
+// #### [ProfileCard.js](./client!components!splash!ProfileCard.js.html)
+// * React api actions
+
+// #### [componentActions.js](./client!constants!componentActions.js.html)
+// * React api actions
+
+// ### CONTAINERS
+
+// #### [Budget.js](./client!containers!Budget.js.html)
+// * React api actions
+
+// #### [ComponentPlayground.js](./client!containers!ComponentPlayground.js.html)
+// * React api actions
+
+// #### [Dashboard.js](./client!containers!Dashboard.js.html)
+// * React api actions
+
+// #### [Goals.js](./client!containers!Goals.js.html)
+// * React api actions
+
+// #### [Map.js](./client!containers!Map.js.html)
+// * React api actions
+
+// #### [Spending.js](./client!containers!Spending.js.html)
+// * React api actions
+
+// #### [Splash.js](./client!containers!Splash.js.html)
+// * React api actions
 
 // ## Back-End
 
