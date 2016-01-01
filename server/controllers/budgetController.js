ffb1f80cd29756eec02571f30cec9a4237dae79e
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-12-31 18:53:40 -0800

    Removed un-used functions from budget controller

--- server/controllers/budgetController.js
+++ server/controllers/budgetController.js
@@ -144,70 +144,6 @@ let budgetController = {
     })
   },
 
-  _updateBudget(userId) {
-    // get budget for text
-    return db.knex('budgets').where({user_id: userId}).select().then((budget) => {
-      // loop through categories in budget
-      return Promise.map(budget, (item, pos) => {
-        // sum transactions for user in each category
-        return budgetController.sumTransactionByCategoryMonthly(item.category_id, userId).then((sum) => {
-          if (sum === item.actual) {
-            // actual is accurate -- no need to update
-            return sum
-          } else {
-            // update actual
-            return db.knex('budgets').where({user_id: userId, category_id: item.category_id}).update({actual: sum}).then((response) => {
-              // if actual is over target in any category
-              if (sum > item.target) {
-                // send text letting user know that they have exceeded their budget for that category
-                // get user for text
-                return budgetController.findUserByID(userId).then((user) => {
-                  // get category name for text
-                  return budgetController.getCategoryName(item.category_id).then((description) => {
-                    // check to see if user has indicated that they would like to recieve text notifications when they go over budget
-                    if (user[0].phone_verified && user[0].text_over_budget) {
-                      apiController.sendMessage('Oink Oink!! \n\nHey ' + user[0].first_name + ' looks like you have gone over your '
-                      + description[0].description + ' budget for this month! \n \n Budget: $' + item.target + ' \n Actual: $' + sum.toFixed(2), user[0].phone_number)
-                    }
-                    return description
-                  })
-                })
-              }
-            })
-          }
-        })
-      })
-    })
-  },
-
-  // TODO: Update Transactions is currently being used for both update and save. May be possible to remove save transactions
-  _saveTransactions(transactions, user_id) {
-
-    // loop over transactions array
-    return Promise.map(transactions, (item) => {
-      // this breaks if transaction doesn't have a category so check if category is definied first
-      if (item.category) {
-        // We are only concerned with first category
-        const category = item.category[0]
-
-        let newCat = new Category({description: category})
-
-        return newCat.fetch().then((category) => {
-          if (category) {
-            //if category already exists, save in database setting all of transaction data plus user_id and category id
-            const category_id = category.id
-            return saveTransaction(item, user_id, category_id)
-          } else {
-            //create a new category
-            return newCat.save().then((category) => {
-              return saveTransaction(item, user_id, category.id)
-            })
-          }
-        })
-      }
-    })
-  },
-
   updateTransactions(transactions, user_id) {
 
     // pull previous transactions for user out of database
@@ -232,19 +168,17 @@ let budgetController = {
         let category = 'Other'
 
         if (transaction.category) {
-
+          
           // We are only concerned with the first category listed
           category = transaction.category[0]
         }
 
         return db.knex('categories').where({description: category}).select().then((result) => {
-
           if (result.length > 0) {
             let category_id = result[0].id
             return saveTransaction(transaction, user_id, category_id)
           } else {
             return db.knex('categories').insert({description: category}).returning('id').then((result) => {
-
               let category_id = result[0].id
               return saveTransaction(transaction, user_id, category_id)
             })
